e4695646022df111771feee5bea505c9a09df70b
brthema072 <brunomarthe@hotmail.com>
2023-10-10 18:27:46 -0300

    Feat: Estrutra de arquivos e páginas

---
+++ src/app/pages/schedule-date.page/schedule-date.page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScheduleDatePageComponent } from './schedule-date.page.component';
+
+describe('ScheduleDatePageComponent', () => {
+  let component: ScheduleDatePageComponent;
+  let fixture: ComponentFixture<ScheduleDatePageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ScheduleDatePageComponent]
+    });
+    fixture = TestBed.createComponent(ScheduleDatePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/schedule-date.page/schedule-date.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-date.page',
+  templateUrl: './schedule-date.page.component.html',
+  styleUrls: ['./schedule-date.page.component.scss']
+})
+export class ScheduleDatePageComponent {
+
+}
