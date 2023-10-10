fce0eff8f07e5837819626853700324cb6d081dd
brthema072 <brunomarthe@hotmail.com>
2023-10-10 18:28:55 -0300

    Feat: Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleDatePageComponent } from './pages/schedule-date.page/schedule-date.page.component';
+import { HomePageComponent } from './pages/home.page/home.page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleDatePageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
