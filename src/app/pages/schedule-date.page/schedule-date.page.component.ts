8b82d49506f8190ae48fb0340ea41e9da15cbcc8
brthema072 <brunomarthe@hotmail.com>
2023-10-11 17:06:57 -0300

    Feat: Build week and montly days

--- src/app/pages/schedule-date.page/schedule-date.page.component.ts
+++ src/app/pages/schedule-date.page/schedule-date.page.component.ts
@@ -8,16 +8,44 @@ import { WorkHoursFacade } from 'src/app/shared/facades/work-hours.facade';
 })
 export class ScheduleDatePageComponent implements OnInit {
   currentDay: string = new Date().toISOString();
-  workDays: string[] = ['1', '2', '3', '4', '5', '6'];
+  workDays: string[] = [
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+    'Domingo',
+  ];
   workHours: string[] = [];
 
   constructor(private workHoursFacade: WorkHoursFacade) {}
 
   ngOnInit(): void {
+    this.buildWorkDatesForHeader();
     this.workHours = this.workHoursFacade.buildHoursOfWork();
   }
 
   schedule(hour: string) {
     console.log(hour);
+  }
+
+  private buildWorkDatesForHeader() {
+    const today = new Date();
+    let nextSevenDays: any[] = [];
+
+    for (let i = 0; i < 7; i++) {
+      const date = new Date(today);
+      date.setDate(today.getDate() + i);
+      const day = this.workDays[date.getDay()];
+
+      const formattedDate = `${date.getDate()}/${
+        date.getMonth() + 1
+      }/${date.getFullYear()}`;
+
+      nextSevenDays.push({ day, date: formattedDate });
+    }
+
+    console.log(nextSevenDays);
   }
 }
