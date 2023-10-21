57cd59349b8ad0faedf16b01d5767dc2df804742
brthema072 <brunomarthe@hotmail.com>
2023-10-21 00:16:37 -0300

    Feat: Open modal by sigin button

--- src/app/pages/home.page/home.page.component.ts
+++ src/app/pages/home.page/home.page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { SimpleModalComponent } from 'src/app/shared/components/simple-modal/simple-modal.component';
+import { SimpleModalService } from 'src/app/shared/services/simple-modal/simple-modal.service';
 
 @Component({
   selector: 'home.page',
   templateUrl: './home.page.component.html',
   styleUrls: ['./home.page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent implements AfterViewInit {
+  @ViewChild('simpleModal') simpleModal!: SimpleModalComponent;
+
+  constructor(private simpleModalService: SimpleModalService) {}
+
+  ngAfterViewInit(): void {
+    this.simpleModalService.setComponent(this.simpleModal);
+  }
+}
--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { SimpleModalService } from '../../services/simple-modal/simple-modal.service';
 
 @Component({
   selector: 'toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  constructor(private simpleModalService: SimpleModalService) {}
+
+  sigin() {
+    this.simpleModalService.getComponent().open();
+  }
+}
