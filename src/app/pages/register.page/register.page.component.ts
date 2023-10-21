44efb90a8209912061ac43f9efea58658fa9b9b0
brthema072 <brunomarthe@hotmail.com>
2023-10-21 09:16:32 -0300

    Feat: Form builer and form group to register page

--- src/app/pages/register.page/register.page.component.ts
+++ src/app/pages/register.page/register.page.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'register.page',
   templateUrl: './register.page.component.html',
   styleUrls: ['./register.page.component.scss'],
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      mobilePhone: [null, [Validators.required]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
