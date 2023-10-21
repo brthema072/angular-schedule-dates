de3a60ef86800eaa145269e9e035f5e0fad262e5
brthema072 <brunomarthe@hotmail.com>
2023-10-21 10:07:56 -0300

    Feat: Using ng content into simple modal

--- src/app/pages/home.page/home.page.component.ts
+++ src/app/pages/home.page/home.page.component.ts
@@ -1,18 +1,8 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { SimpleModalComponent } from 'src/app/shared/components/simple-modal/simple-modal.component';
-import { SimpleModalService } from 'src/app/shared/services/simple-modal/simple-modal.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'home.page',
   templateUrl: './home.page.component.html',
   styleUrls: ['./home.page.component.scss'],
 })
-export class HomePageComponent implements AfterViewInit {
-  @ViewChild('simpleModal') simpleModal!: SimpleModalComponent;
-
-  constructor(private simpleModalService: SimpleModalService) {}
-
-  ngAfterViewInit(): void {
-    this.simpleModalService.setComponent(this.simpleModal);
-  }
-}
+export class HomePageComponent {}
--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { SimpleModalService } from '../../services/simple-modal/simple-modal.service';
+import { SimpleModalComponent } from '../simple-modal/simple-modal.component';
 
 @Component({
   selector: 'toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements AfterViewInit {
+  @ViewChild('simpleModal') simpleModal!: SimpleModalComponent;
+
   constructor(private simpleModalService: SimpleModalService) {}
+
+  ngAfterViewInit(): void {
+    this.simpleModalService.setComponent(this.simpleModal);
+  }
 
   sigin() {
     this.simpleModalService.getComponent().open();
