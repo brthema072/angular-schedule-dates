14682a252af1e27cbf172d51393883f56a3c1bf8
brthema072 <brunomarthe@hotmail.com>
2023-10-27 15:18:18 -0300

    Refactor: Week view from schedule page

--- src/app/pages/schedule-date.page/schedule-date.page.component.ts
+++ src/app/pages/schedule-date.page/schedule-date.page.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { WorkDaysFacade } from 'src/app/pages/schedule-date.page/facades/work-days.facade';
-import { WorkHoursFacade } from 'src/app/pages/schedule-date.page/facades/work-hours.facade';
-import {
-  ModalScheduleDateComponent,
-  SelectedDate,
-} from './components/modal-schedule-date/modal-schedule-date.component';
+import { Component } from '@angular/core';
 import { toggleOption } from 'src/app/shared/components/simple-toggle/simple-toggle.component';
 
 @Component({
@@ -12,9 +6,7 @@ import { toggleOption } from 'src/app/shared/components/simple-toggle/simple-tog
   templateUrl: './schedule-date.page.component.html',
   styleUrls: ['./schedule-date.page.component.scss'],
 })
-export class ScheduleDatePageComponent implements OnInit {
-  @ViewChild('modal') modal!: ModalScheduleDateComponent;
-
+export class ScheduleDatePageComponent {
   currentDay: string = new Date().toLocaleDateString('pt-Br');
   workDaysByWeek: string[] = [
     'Segunda-feira',
@@ -24,11 +16,9 @@ export class ScheduleDatePageComponent implements OnInit {
     'Sexta-feira',
     'Sábado',
   ];
+
   workDaysByMonth: string[] = [];
   workHours: string[] = [];
-
-  scheduleDay: string = '';
-  scheduleHour: string = '';
 
   toggleValues: Array<Partial<toggleOption>> = [
     { name: 'Semana', select: true },
@@ -36,35 +26,8 @@ export class ScheduleDatePageComponent implements OnInit {
     { name: 'Ano', select: false },
   ];
 
-  constructor(
-    private workHoursFacade: WorkHoursFacade,
-    private workDaysFacade: WorkDaysFacade
-  ) {}
-
-  ngOnInit(): void {
-    this.workDaysByMonth = this.workDaysFacade.buildWorkDatesForHeader(
-      this.workDaysByWeek
-    );
-
-    this.workHours = this.workHoursFacade.buildHoursOfWork();
-  }
-
-  openModal(day: string, hour: string) {
-    this.modal.selectedDate = `${day} as ${hour}`;
-    this.modal.day = day;
-    this.modal.hour = hour;
-
-    this.modal.simpleModal.open();
-  }
-
   getWeekDayName(): string {
     return this.workDaysByWeek[new Date().getDay() - 1];
-  }
-
-  schedule(selectedDate: SelectedDate) {
-    this.scheduleDay = selectedDate.day;
-    this.scheduleHour = selectedDate.hour;
-    console.log(this.scheduleDay, this.scheduleHour);
   }
 
   getToggleSelected(toggle: toggleOption) {
