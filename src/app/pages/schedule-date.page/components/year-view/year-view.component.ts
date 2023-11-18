85630625b6e7144f2f1f52577d3981fdd19f51dd
brthema072 <brunomarthe@hotmail.com>
2023-11-18 00:43:13 -0300

    Feat: Year view from schedule page

--- src/app/pages/schedule-date.page/components/year-view/year-view.component.ts
+++ src/app/pages/schedule-date.page/components/year-view/year-view.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'year-view',
   templateUrl: './year-view.component.html',
   styleUrls: ['./year-view.component.scss'],
 })
-export class YearViewComponent {}
+export class YearViewComponent implements OnInit {
+  currentYear: number = new Date().getFullYear();
+
+  years: number[] = [];
+  selectedYear: number = 0;
+
+  ngOnInit(): void {
+    this.selectedYear = this.currentYear;
+
+    for (let i = 1; i <= 2; i++) {
+      this.years.push(this.currentYear - i);
+    }
+
+    for (let i = 0; i < 1; i++) {
+      this.years.push(this.currentYear + i);
+    }
+
+    this.years = this.years.sort();
+  }
+}
